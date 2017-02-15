421aea00813a91315a14ce8d38f6e83257a275b5
Harry Roberts <csswizardry@gmail.com>
2017-02-15 07:42:32 +0000

    Enable pagination at top and bottom of posts list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0017';
+var cacheName = 'csswizardry:0018';
 var cacheFiles = [
   '/',
   '/about/',
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
 // Empty out any caches that don’t match the ones listed.
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['csswizardry:0017'];
+  var cacheWhitelist = ['csswizardry:0018'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
