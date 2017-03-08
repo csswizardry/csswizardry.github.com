8a620a03a2fa9680ac38ea1d7f9b342f42716b83
Harry Roberts <csswizardry@gmail.com>
2017-03-08 22:21:54 +0100

    Update speaking

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0023';
+var cacheName = 'csswizardry:0024';
 var cacheFiles = [
   '/',
   '/about/',
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
 // Empty out any caches that don’t match the ones listed.
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['csswizardry:0023'];
+  var cacheWhitelist = ['csswizardry:0024'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
