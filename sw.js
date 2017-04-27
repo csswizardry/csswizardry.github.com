f7ec404cee59726e84a759d59e21ff4801385bce
Harry Roberts <csswizardry@gmail.com>
2017-04-27 19:34:00 +0100

    Experiment with sticky header

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0038';
+var cacheName = 'csswizardry:0039';
 var cacheFiles = [
   '/',
   '/about/',
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
 // Empty out any caches that don’t match the ones listed.
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['csswizardry:0038'];
+  var cacheWhitelist = ['csswizardry:0039'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
