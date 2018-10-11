05c2840b6a821dfc0e0356c9cb1b9e5d65ca27f4
Harry Roberts <csswizardry@gmail.com>
2018-10-11 13:21:38 +0300

    Bump SW

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0077';
+var cacheName = 'csswizardry:0078';
 var cacheFiles = [
   '/',
   '/about/',
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
 // Empty out any caches that don’t match the ones listed.
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['csswizardry:0077'];
+  var cacheWhitelist = ['csswizardry:0078'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
