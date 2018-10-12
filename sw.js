11ba62f059dafb9ea40b058fc2aa320de05e0674
Harry Roberts <csswizardry@gmail.com>
2018-10-12 16:17:00 +0300

    Add new client logo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0078';
+var cacheName = 'csswizardry:0079';
 var cacheFiles = [
   '/',
   '/about/',
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
 // Empty out any caches that don’t match the ones listed.
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['csswizardry:0078'];
+  var cacheWhitelist = ['csswizardry:0079'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
