1f5541c3d8268976989c4be4b8d132145823b450
Harry Roberts <csswizardry@gmail.com>
2019-11-13 00:21:34 +0100

    Update Masthead URLs
    I kept on getting masthead references wrong because I’m not particularly
    smart. This commit hashes them (manually, admittedly) in the hopes I’ll
    be immune to problems for the next five or so years. We’ll see.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0115';
+var cacheName = 'csswizardry:0116';
 var cacheFiles = [
   '/',
   '/about/',
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
 // Empty out any caches that don’t match the ones listed.
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['csswizardry:0115'];
+  var cacheWhitelist = ['csswizardry:0116'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
