d3469ca2940fa4d861cabbdc52710342969ce8d6
Harry Roberts <csswizardry@gmail.com>
2020-09-24 15:46:04 +0100

    Update LUX

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0129';
+var cacheName = 'csswizardry:0130';
 var cacheFiles = [
   '/',
   '/about/',
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
 // Empty out any caches that don’t match the ones listed.
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['csswizardry:0129'];
+  var cacheWhitelist = ['csswizardry:0130'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
