8d66d76fa0f02cc22d3df192cdfed1d36fd2a241
Harry Roberts <csswizardry@gmail.com>
2020-09-28 23:05:01 +0100

    Experiment with prerender

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0130';
+var cacheName = 'csswizardry:0131';
 var cacheFiles = [
   '/',
   '/about/',
@@ -53,7 +53,7 @@ self.addEventListener('fetch', function(event) {
 // Empty out any caches that don’t match the ones listed.
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['csswizardry:0130'];
+  var cacheWhitelist = ['csswizardry:0131'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
