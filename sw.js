c2f12683ebf03bb450a4ae3e9f08ce529e1e26ac
Harry Roberts <csswizardry@gmail.com>
2025-02-11 17:11:45 +0000

    Move blog pagination under /blog dir

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'csswizardry:0164';
+var cacheName = 'csswizardry:0165';
 var cacheFiles = [
   '/',
   '/about/',
@@ -63,7 +63,7 @@ self.addEventListener('fetch', function(event) {
 // Empty out any caches that don’t match the ones listed.
 self.addEventListener('activate', function(event) {
 
-  var cacheWhitelist = ['csswizardry:0164'];
+  var cacheWhitelist = ['csswizardry:0165'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
