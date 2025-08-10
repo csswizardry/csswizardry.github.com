d2f550f857819d59c9670634dbaa0bd6bc66183d
Harry Roberts <csswizardry@gmail.com>
2025-08-10 20:09:49 +0100

    Externalise @SpeedCurve

---
+++ js/speedcurve.js
@@ -0,0 +1,30 @@
+/**
+ * All the data previously captured by Obs.js is now sent to SpeedCurve!
+ *
+ * I ❤️ SpeedCurve!
+ */
+
+(() => {
+  // Bail out if SpeedCurve is not available.
+  if (!window.LUX || typeof window.LUX.addData !== 'function') return;
+
+  const obs = window.obs || Object.create(null);
+
+  // Keys we intend to send. Keep in sync with obs.js
+  const keys = [
+    'dataSaver',
+    'rttBucket',
+    'rttCategory',
+    'downlinkBucket',
+    'downlinkMax',
+    'batteryCritical',
+    'batteryLow',
+    'batteryCharging',
+  ];
+
+  for (const key of keys) {
+    if (Object.prototype.hasOwnProperty.call(obs, key)) {
+      window.LUX.addData(key, obs[key]);
+    }
+  }
+})();
